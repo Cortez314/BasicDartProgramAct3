3e32618233eb3d1a8e2fe2ac9bc46e22a676624a
Cortez314 <hannycortez314@gmail.com>
2025-08-08 11:14:25 +0000

    Dart simple calculator program.

---
+++ Calculator.dart
@@ -0,0 +1,9 @@
+void main() {
+  print('---- Simple calculator program ----\n');
+  
+  int num1 = 45;
+  int num2 = 15;
+
+  print('Number 1: $num1, Number 2: $num2\n');
+  print('Addition: ${num1 + num2}, Subtraction: ${num1 - num2}, Multiplication: ${num1 * num2}, Division: ${num1 / num2}');
+}
